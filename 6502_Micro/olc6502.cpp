ecb11f5607635399136effaf97f38a88fdd5f159
HakamAtassi <atassi.hakam@gmail.com>
2022-04-29 02:00:05 -0400

    half way through implementation

--- 6502_Micro/olc6502.cpp
+++ 6502_Micro/olc6502.cpp
@@ -1,12 +1,18 @@
 #include "headers/olc6502.h"
 #include <cstdint>
+#include <sys/types.h>
 #include "headers/Bus.h"
 
 
 olc6502::olc6502()
 {
-	using a=olc6502;
-		lookup = 
+	using a=olc6502; //to make code more readable. a is an olc6502 object. 
+	//helps make defining function pointers easier. 
+		
+	//lookup is essentially a table with all possible opcodes of the micro. It stores all data outlined in INSTRUCTION. 
+	//NOTE: "???" are invalid opcodes.
+
+	lookup = 
 	{
 		{ "BRK", &a::BRK, &a::IMM, 7 },{ "ORA", &a::ORA, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "ORA", &a::ORA, &a::ZP0, 3 },{ "ASL", &a::ASL, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PHP", &a::PHP, &a::IMP, 3 },{ "ORA", &a::ORA, &a::IMM, 2 },{ "ASL", &a::ASL, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ABS, 4 },{ "ASL", &a::ASL, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
 		{ "BPL", &a::BPL, &a::REL, 2 },{ "ORA", &a::ORA, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ZPX, 4 },{ "ASL", &a::ASL, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLC", &a::CLC, &a::IMP, 2 },{ "ORA", &a::ORA, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ABX, 4 },{ "ASL", &a::ASL, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
@@ -25,10 +31,9 @@ olc6502::olc6502()
 		{ "CPX", &a::CPX, &a::IMM, 2 },{ "SBC", &a::SBC, &a::IZX, 6 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "CPX", &a::CPX, &a::ZP0, 3 },{ "SBC", &a::SBC, &a::ZP0, 3 },{ "INC", &a::INC, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "INX", &a::INX, &a::IMP, 2 },{ "SBC", &a::SBC, &a::IMM, 2 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::SBC, &a::IMP, 2 },{ "CPX", &a::CPX, &a::ABS, 4 },{ "SBC", &a::SBC, &a::ABS, 4 },{ "INC", &a::INC, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
 		{ "BEQ", &a::BEQ, &a::REL, 2 },{ "SBC", &a::SBC, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "SBC", &a::SBC, &a::ZPX, 4 },{ "INC", &a::INC, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SED", &a::SED, &a::IMP, 2 },{ "SBC", &a::SBC, &a::ABY, 4 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "SBC", &a::SBC, &a::ABX, 4 },{ "INC", &a::INC, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
 	};
-
-
-
 }
+
+
 
 olc6502::~olc6502()
 {
@@ -44,4 +49,194 @@ uint8_t olc6502::read(uint16_t a)
 void olc6502::write(uint16_t address,uint8_t d)
 {
 	bus->write(address,d);
+}
+
+
+void olc6502::clock()
+{
+	/**cycles variable stores the number of requires clock cycles. if its greater than 0, it will do the operation, but the next few calls wont do anything until cycles is back to 0.**/
+
+	if(cycles==0) //no cycles have occured.
+	{
+		opcode=read(pc);
+		pc++;
+		
+		cycles=lookup[opcode].cycles;
+		uint8_t additional_cycle1 = (this->*lookup[opcode].addrmode)(); //calls addrmode
+		//"this" has pointer called lookup that is addressed by opcode number. 
+		//that vector stores a function pointer that is used to call the (corrispodning) addrmode function. 
+
+		uint8_t additional_cycle2 = (this->*lookup[opcode].operate)();
+
+		cycles +=(additional_cycle2 & additional_cycle1);
+
+	}
+
+	cycles--;
+
+}
+
+void olc6502::SetFlag(FLAGS6502 f, bool v)
+{
+	if(v) //if v is 1, set flag (toggle on)
+	{
+		status |=f;
+	}
+	else //if v is 0, reset flag (toggle of)
+	{
+		status &= ~f; //0b1111 & !0001 = 1110, (toggle off bit mask) 
+	}
+}
+
+
+// addressing modes
+
+uint8_t olc6502::IMP() //implied addressing directly stores the data into the accumulator "a" 
+{
+	fetched =a;
+	return 0;
+}
+uint8_t olc6502::IMM() 
+{
+	addr_abs = pc++; //The address of the data is stored in memory directly
+	//fetch address then post increment
+	return 0;
+}
+
+
+//Zero page addressing
+//0xFF55 as an example
+//8 MSB are referred to as "page"
+//8 LSB are rererred to as "offset"
+//hence, memory can be mapped as 256 pages, 256 offsets
+//
+uint8_t olc6502::ZP0() //used for addressing first 256 bytes of data in the system. 
+{
+	addr_abs = read(pc); 
+	pc++;
+	addr_abs &=0x00FF; //clear first t bytes since it can only be in first page
+	return 0;
+}
+
+uint8_t olc6502::ZPX() 
+{
+	addr_abs=(read(pc)+x); //x is x register on processor 
+												// add value in x to contents of read memory
+	pc++; 								
+	addr_abs &=0x00FF;
+
+	//this one is slightly tricky. The pc points to a memory address. That memory address could be, for example, the address to an array. 
+	//To index each element in that array, the value "X" can be added to that original addess fetched from PC. 
+	//This effectivly enables addressing values indexed away from an original base address
+	//This is exactly of arrays work in C++
+
+	return 0;
+}
+
+uint8_t olc6502::ZPY() 
+{
+
+	//read explination for ZPX. Its the same
+	addr_abs=(read(pc)+y); //y is y register on processor
+												// add value in y to contents of read memory
+	pc++;
+	addr_abs &=0x00FF;
+	return 0;
+}
+
+uint8_t olc6502::ABS()
+{
+
+	//if the instruction directly refers to the opcode, it wont fit in the standard instruction size
+	//This is since memory is only 16 bits wide, but an address is 16 bits on its own.
+	//This is not a problem in the above addressing modes since the address is discovered after that instruction is fetched
+	//But if you want the full address in the instruction, absolute addressing si required. 
+
+	uint16_t lo=read(pc);
+	pc++;
+	uint16_t hi=read(pc);
+	pc++;
+
+	addr_abs=(hi<<8) | lo;
+
+
+	//this functions as follows:
+	//go to memory location => you are told to fetch data from specific address
+	//that place in memory will store the lower 8 bits. The next place in memory will store the upper 8 bits. 
+	//PS. the 6502 is little endian. 
+	return 0;
+}
+
+
+uint8_t olc6502::ABX()
+{
+
+	//Very similar to ZPX. It also increments a value "x" from a base address. 	//The only difference is that, instead of incrementing x from the pc, you increment from an absolute address provided in the instruction. 
+
+	uint8_t lo =read(pc);
+	pc++;
+	uint16_t hi = read(pc);
+	pc++;
+
+	addr_abs = (hi<<8) | lo;
+	addr_abs +=x;
+
+	if((addr_abs & 0xFF00) != (hi<<8)) //return 1 if a new memory page is entered
+	{
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+
+}
+
+
+uint8_t olc6502::ABY()
+{
+	//see above explination. Same applies but for Y register
+	uint8_t lo =read(pc);
+	pc++;
+	uint16_t hi = read(pc);
+	pc++;
+
+	addr_abs = (hi<<8) | lo;
+	addr_abs +=y;
+
+	if((addr_abs & 0xFF00) != (hi<<8))
+	{
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+
+}
+
+
+uint8_t olc6502::IND()
+{
+
+
+	uint16_t ptr_lo =read(pc);
+	pc++;
+	uint16_t ptr_hi=read(pc);
+	pc++;
+
+	uint16_t ptr = (ptr_hi << 8 ) | ptr_lo;
+
+	if (ptr_lo == 0x00FF)
+	{
+		addr_abs=(read(ptr & 0xFF00)<<8) | read(ptr+0);
+	}
+	else 
+	{
+		addr_abs =(read(ptr +1)<<8) | read (ptr+0);
+	}
+
+
+	
+	return 0;
 }
